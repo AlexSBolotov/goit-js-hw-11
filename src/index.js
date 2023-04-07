d98c0b153c8cc0f851822e79403b6139a800f008
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-04-07 22:13:56 +0300

    qwet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './css/styles.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix';
 import { CardsAPI } from './cardsAPI.js';
 import galleryCardTmplt from './templates/galleryCardTmplt.hbs';
